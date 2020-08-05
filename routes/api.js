793c230beb4bc82f3dadbc32ad938db50e258c3f
Juan Osorio <juan.michael.osorio@gmail.com>
2020-08-05 13:36:25 -0500

    established a 5 day range for '/api/workouts/range'

--- routes/api.js
+++ routes/api.js
@@ -2,42 +2,53 @@ const Workout = require("../models/workoutModels");
 
 exports.route = (app) => {
     //getLastWorkout
-    app.get("/api/workouts", ({body}, res) => {
+    app.get("/api/workouts", ({ body }, res) => {
         Workout.find({})
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err => {
-            res.status(400).json(err);
-        });
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            });
     });
 
     //addExercise
-    app.put("/api/workouts/:id", ({body}, res) => {
-        Workout.updateOne(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err =>{
-            res.status(400).json(err);
-        })
+    app.put("/api/workouts/:id", (req, res) => {
+        const id = req.params.id;
+        Workout.findByIdAndUpdate(id, req.body)
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            })
     });
 
     //createWorkout
-    app.post("/api/workouts", ({body}, res) => {
+    app.post("/api/workouts", ({ body }, res) => {
         Workout.create(body)
-        .then(dbWorkout => {
-            res.json(dbWorkout);
-        })
-        .catch(err =>{
-            res.status(400).json(err);
-        })
+            .then(dbWorkout => {
+                res.json(dbWorkout);
+            })
+            .catch(err => {
+                res.status(400).json(err);
+            })
     });
 
     //getWorkoutsInRange
-    app.get('/api/workouts/range', ({body}, res) =>{
+    app.get('/api/workouts/range', ({ body }, res) => {
+        Workout.find({
+            'day':
+            {
+                $lte: new Date().setDate(new Date().getDate()),
+                $gte: new Date().setDate(new Date().getDate()-5)
+            }
+        })
+        .then(workoutdb => {
+            res.json(workoutdb);
+            console.log(workoutdb);
+        });
+    });
 
-    })
 
-  
 }
